9e88fb1a64168c041e66a8b5375daba0112b0031
wsc-0816 <soonkwon@163.com>
2016-07-19 22:28:12 +0800

    update

---
+++ start/4-class.cpp
@@ -0,0 +1,11 @@
+// This file is about the class 
+// class is a data type which defined by users.
+/*
+If you want to define a class, you must konw what it is and what it is for.
+Example :
+A class is all the students of the school, one of studenta is a specific object .
+For the students, you can let them to take a Finance class rather not a math class.
+This is one of the actions, you can let them do. So this is a function.
+
+ 
+*/
